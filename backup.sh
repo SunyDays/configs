0eecbe126e200e93a9edd68c9d9f9116dd515be4
SunyDays <sunnyddayss@gmail.com>
2017-02-12 13:29:33 +0300

    rename configs and modify scripts

--- backup.sh
+++ backup.sh
@@ -6,12 +6,13 @@ if [ -z "$1" ]; then
 fi
 
 echo backup configs
-cp ~/.bashrc ./
-cp ~/.emacs ./
-cp ~/.gitconfig ./
+cp ~/.bashrc ./bashrc
+cp ~/.emacs ./emacs
+cp ~/.gitconfig ./gitconfig
 cp ~/.config/awesome/rc.lua ./awesome_conf.lua
-cp ~/.config/systemd/user/default.target.wants/emacs.service ./
+# cp ~/.config/systemd/user/default.target.wants/emacs.service ./
 cp ~/.config/terminator/config ./terminator.config
+cp ~/.config/zathura/zathurarc
 
 echo backup pacman database
 tar -cjf pacman_database.tar.bz2 /var/lib/pacman/local
--- restore.sh
+++ restore.sh
@@ -3,17 +3,18 @@
 git pull
 
 echo restore configs
-cp ./.bashrc ~/
-cp ./.emacs ~/
-cp ./.gitconfig ~/
+cp ./bashrc ~/.bashrc
+cp ./emacs ~/.emacs
+cp ./gitconfig ~/gitconfig
 cp ./awesome_conf.lua ~/.config/awesome/rc.lua
 cp ./terminator.config ~/.config/terminator/config
+cp ./zathurarc ~/.config/zathura/
 
-cp ./emacs.service ~/.config/systemd/user/default.target.wants/
-systemctl --user stop emacs
-systemctl --user reload emacs
-systemctl --user enable emacs
-systemctl --user start emacsDE
+# cp ./emacs.service ~/.config/systemd/user/default.target.wants/
+# systemctl --user stop emacs
+# systemctl --user reload emacs
+# systemctl --user enable emacs
+# systemctl --user start emacsDE
 
 echo resotre pacman database
 cp pacman_database.tar.bz2 /
