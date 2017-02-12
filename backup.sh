1d8d1e84aa6a8443d34f815f06ec3b8b7b286199
SunyDays <sunnyddayss@gmail.com>
2017-02-12 13:54:11 +0300

    modify backup script

--- backup.sh
+++ backup.sh
@@ -10,9 +10,10 @@ cp ~/.bashrc ./bashrc
 cp ~/.emacs ./emacs
 cp ~/.gitconfig ./gitconfig
 cp ~/.config/awesome/rc.lua ./awesome_conf.lua
-# cp ~/.config/systemd/user/default.target.wants/emacs.service ./
 cp ~/.config/terminator/config ./terminator.config
 cp ~/.config/zathura/zathurarc ./
+cp ~/.offlineimaprc ./offlineimaprc
+cp ~/.offlineimap.py ./offlineimap.py
 
 echo backup pacman database
 tar -cjf pacman_database.tar.bz2 /var/lib/pacman/local
