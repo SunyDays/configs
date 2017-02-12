4997e6f983fc55dfa43cdefa411a760208208952
SunyDays <sunnyddayss@gmail.com>
2017-02-12 14:02:27 +0300

    adding authinfo

--- backup.sh
+++ backup.sh
@@ -14,6 +14,7 @@ cp ~/.config/terminator/config ./terminator.config
 cp ~/.config/zathura/zathurarc ./
 cp ~/.offlineimaprc ./offlineimaprc
 cp ~/.offlineimap.py ./offlineimap.py
+cp ~/.authinfo.gpg ./authinfo.gpg
 
 echo backup pacman database
 tar -cjf pacman_database.tar.bz2 /var/lib/pacman/local
