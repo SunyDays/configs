24994302c238a050237274cf5391493c0ad21ec7
SunyDays <sunnyddayss@gmail.com>
2017-02-12 13:32:20 +0300

    adding zathura config

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ cp ~/.gitconfig ./gitconfig
 cp ~/.config/awesome/rc.lua ./awesome_conf.lua
 # cp ~/.config/systemd/user/default.target.wants/emacs.service ./
 cp ~/.config/terminator/config ./terminator.config
-cp ~/.config/zathura/zathurarc
+cp ~/.config/zathura/zathurarc ./
 
 echo backup pacman database
 tar -cjf pacman_database.tar.bz2 /var/lib/pacman/local
